171e021f2b0193c68afcb398e23380e274ea6840
limormekaiten <limor.mekaiten@gmail.com>
2022-04-10 22:34:13 +0300

    search frontend dib

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,13 +1,31 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
+import agent from "../../agent";
+const Banner = (props) => {
+  const onSearchChange = (event) => {
+    props.onSearchFilter(
+      event.target.value,
+      (page) => agent.Items.byTitle(event.target.value),
+      agent.Items.byTitle(event.target.value)
+    );
+  };
 
-const Banner = () => {
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
           <span id="get-part">A place to get</span>
+          <form>
+            <input
+              style={{ width: "250px" }}
+              type="text"
+              placeholder="what is there you truly desire?"
+              name="term"
+              id="search-box"
+              onChange={onSearchChange}
+            />
+          </form>
           <span> the cool stuff.</span>
         </div>
       </div>
